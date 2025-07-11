81db77d7d2f471bd90abe2080b1f4d8f842b87ce
JESSADA <cha02411@gmail.com>
2025-07-11 15:09:45 +0700

    test try

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -111,7 +111,7 @@ class SensorController extends Controller
             ], 422);
         }
 
-        return response()->json($userSenosr);
+        return response()->json($useUserSensor);
 
         Sensor::create([
             'user_id' => $request->input('user_id'),
