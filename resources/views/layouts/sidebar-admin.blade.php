ec6f7c07bec546dcfbe52fdf448320146b747249
JESSADA <cha02411@gmail.com>
2025-07-12 02:33:31 +0700

    logo admin

--- resources/views/layouts/sidebar-admin.blade.php
+++ resources/views/layouts/sidebar-admin.blade.php
@@ -9,17 +9,18 @@
 </style>
 <!-- ✅ MOBILE NAVBAR -->
 <nav class="lg:hidden fixed top-0 left-0 w-full bg-gradient-to-r from-green-500 via-emerald-400 to-cyan-500 text-white flex items-center justify-between px-4 py-3 shadow z-30">
-    <div class="text-xl font-bold">Agrosector Back</div>
+    <div>
+        <img src="{{ asset('/images/logo_sidebar.jpg') }}" alt="logo" width="150">
+    </div>
     <button id="open-sidebar" class="text-3xl text-white">☰</button>
 </nav>
 
 <!-- ✅ OVERLAY SIDEBAR (mobile only) -->
-<aside id="mobile-sidebar"
-    class="fixed top-0 left-0 h-full w-64 bg-gradient-to-b from-green-500 via-emerald-400 to-cyan-400 text-white transform -translate-x-full transition-transform duration-300 ease-in-out z-40 lg:hidden shadow-lg">
-
+<aside id="mobile-sidebar"  class="fixed top-0 left-0 h-full w-64 bg-gradient-to-b from-green-500 via-emerald-400 to-cyan-400 text-white transform -translate-x-full transition-transform duration-300 ease-in-out z-40 lg:hidden shadow-lg">
     <button id="close-sidebar" class="absolute top-4 right-4 text-3xl text-white">×</button>
-
-    <div class="text-center text-2xl font-bold mt-12 mb-6">Agrosector Back</div>
+    <div class="flex justify-center my-5">
+        <img src="{{ asset('/images/logo_sidebar.jpg') }}" alt="logo" width="180">
+    </div>
     <nav class="flex flex-col space-y-2 px-4">
         <a href="{{ route('admin.index') }}" class="{{ (isset($sideActive) && $sideActive == 'map') ? 'side-active' : '' }} px-4 py-2 hover:bg-white hover:text-green-800 rounded transition">
             <i class="fa-solid fa-map"></i> แผนที่
@@ -47,7 +48,9 @@
 
 <!-- ✅ DESKTOP SIDEBAR -->
 <aside class="hidden lg:flex flex-col w-64 h-full fixed top-0 left-0 bg-gradient-to-b from-green-500 via-emerald-400 to-cyan-400 text-white p-4 space-y-4 shadow-lg z-20">
-    <div class="text-center text-2xl font-bold">Agrosector Back</div>
+    <div class="flex justify-center my-3">
+        <img src="{{ asset('/images/logo_sidebar.jpg') }}" alt="logo" width="250">
+    </div>
     <nav class="flex flex-col space-y-2">
         <a href="{{ route('admin.index') }}" class="hover:bg-white hover:text-green-800 px-4 py-2 rounded transition {{ (isset($sideActive) && $sideActive == 'map') ? 'side-active' : '' }}">
             <i class="fa-solid fa-map"></i> Map
