6e5fe2af3a42d7d951c504e043265b385bcd2258
JESSADA <cha02411@gmail.com>
2025-08-13 18:07:25 +0700

    logo

--- resources/views/chart.blade.php
+++ resources/views/chart.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard Sensor</title>
+     <link href="{{ asset('/images/logo.png') }}" rel="shortcut icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
